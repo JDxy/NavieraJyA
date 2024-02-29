c8e369a07c0542907b73c4e37cfa84fe8538fd9c
Jose David Falcon Sosa <90795629+JDxy@users.noreply.github.com>
2024-02-29 11:27:42 +0100

    test

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.proyecto.navierajya {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.proyecto.navierajya to javafx.fxml;
-    exports com.proyecto.navierajya;
-}
